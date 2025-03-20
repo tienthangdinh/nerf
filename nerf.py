fdd2185a225a9e4e6dffb15668fb45b8a52f76a3
Tien Thang Dinh <tienthangdinh@hotmail.com>
2025-03-20 23:42:58 +0100

    ray rendering definition

--- nerf.py
+++ nerf.py
@@ -24,3 +24,55 @@ class NerfModel(nn.Module):
         self.embedding_dim_pos = embedding_dim_pos
         self.embedding_dim_direction = embedding_dim_direction
         self.relu = nn.ReLU()
+
+    @staticmethod
+    def positional_encoding(x, L):
+        out = [x]
+        for j in range(L):
+            out.append(torch.sin(2 ** j * x))
+            out.append(torch.cos(2 ** j * x))
+        return torch.cat(out, dim=1)
+
+    def forward(self, o, d): #o and d defined from this line: nerf_model(x.reshape(-1, 3), ray_directions.reshape(-1, 3))
+        emb_x = self.positional_encoding(o, self.embedding_dim_pos) # emb_x: [batch_size, embedding_dim_pos * 6]
+        emb_d = self.positional_encoding(d, self.embedding_dim_direction) # emb_d: [batch_size, embedding_dim_direction * 6]
+        h = self.block1(emb_x) # h: [batch_size, hidden_dim]
+        tmp = self.block2(torch.cat((h, emb_x), dim=1)) # tmp: [batch_size, hidden_dim + 1]
+        h, sigma = tmp[:, :-1], self.relu(tmp[:, -1]) # h: [batch_size, hidden_dim], sigma (the last element for density): [batch_size]
+        h = self.block3(torch.cat((h, emb_d), dim=1)) # h: [batch_size, hidden_dim // 2]
+        c = self.block4(h) # c: [batch_size, 3]
+        return c, sigma
+    
+def compute_accumulated_transmittance(alphas):
+    accumulated_transmittance = torch.cumprod(alphas, 1)
+    return torch.cat((torch.ones((accumulated_transmittance.shape[0], 1), device=alphas.device),
+                      accumulated_transmittance[:, :-1]), dim=-1)
+
+
+def render_rays(nerf_model, ray_origins, ray_directions, hn=0, hf=0.5, nb_bins=192):
+    device = ray_origins.device
+    
+    t = torch.linspace(hn, hf, nb_bins, device=device).expand(ray_origins.shape[0], nb_bins)
+    # Perturb sampling along each ray.
+    mid = (t[:, :-1] + t[:, 1:]) / 2.
+    lower = torch.cat((t[:, :1], mid), -1)
+    upper = torch.cat((mid, t[:, -1:]), -1)
+    u = torch.rand(t.shape, device=device)
+    t = lower + (upper - lower) * u  # [batch_size, nb_bins]
+    delta = torch.cat((t[:, 1:] - t[:, :-1], torch.tensor([1e10], device=device).expand(ray_origins.shape[0], 1)), -1)
+
+    # Compute the 3D points along each ray
+    x = ray_origins.unsqueeze(1) + t.unsqueeze(2) * ray_directions.unsqueeze(1)   # [batch_size, nb_bins, 3]
+    # Expand the ray_directions tensor to match the shape of x
+    ray_directions = ray_directions.expand(nb_bins, ray_directions.shape[0], 3).transpose(0, 1) 
+
+    colors, sigma = nerf_model(x.reshape(-1, 3), ray_directions.reshape(-1, 3))
+    colors = colors.reshape(x.shape)
+    sigma = sigma.reshape(x.shape[:-1])
+
+    alpha = 1 - torch.exp(-sigma * delta)  # [batch_size, nb_bins]
+    weights = compute_accumulated_transmittance(1 - alpha).unsqueeze(2) * alpha.unsqueeze(2)
+    # Compute the pixel values as a weighted sum of colors along each ray
+    c = (weights * colors).sum(dim=1)
+    weight_sum = weights.sum(-1).sum(-1)  # Regularization for white background 
+    return c + 1 - weight_sum.unsqueeze(-1)
